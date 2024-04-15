15778be0f5091db7dd5846e6e4629dfcfbeb1e3b
Tulio <tuliocsaraiva@gmail.com>
2024-04-15 12:00:37 -0300

    criando layout do SignUp

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div>
+      <form className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-6'>
+            <div className='col text-center'>
+              <h1>Conectar</h1>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+              <div id="emailHelp" class="form-text">Nós nunca iremos compartilhar o seu email com ninguém! </div>
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Senha</label>
+              <input type="password" class="form-control" id="exampleInputPassword1"/>
+            </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <div className='col text-center'>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
