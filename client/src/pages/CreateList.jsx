b662d21e5c1fd46db793be54ad814828a5a29b4b
Tulio <tuliocsaraiva@gmail.com>
2024-05-08 09:15:12 -0300

    adicionando mais funcionalidade de criação de lisgaem

--- client/src/pages/CreateList.jsx
+++ client/src/pages/CreateList.jsx
@@ -7,16 +7,33 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-import { set } from 'mongoose';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateList() {
-    const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     imageUrls: [],
+    name: '',
+    description: '',
+    address: '',
+    type: 'rent',
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
   });
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   console.log(formData);
+
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       setUploading(true);
@@ -70,6 +87,68 @@ export default function CreateList() {
     });
   };
 
+  const handleChange = (e) => {
+    if (e.target.id === 'sale' || e.target.id === 'rent') {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === 'parking' ||
+      e.target.id === 'furnished' ||
+      e.target.id === 'offer'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === 'number' ||
+      e.target.type === 'text' ||
+      e.target.type === 'textarea'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError('You must upload at least one image');
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError('Discount price must be lower than regular price');
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   const handleRemoveImage = (index) => {
     setFormData({
       ...formData,
@@ -78,56 +157,93 @@ export default function CreateList() {
   };
 
   return (
-    <section className='container text-center mt-3'>
+    <form onSubmit={handleSubmit} className='container text-center mt-3'>
         <div className="row ">
             <h1 className='mb-3'>Criando uma listagem</h1>
             <div className='col' >
 
                 {/* Dados básicos */}
-                <input className='w-100 mb-2 rounded' type="text" placeholder='Nome' id='name' maxLength='10' minLength='50' required/>
-                <textarea className='w-100 mb-2 rounded' type="text" placeholder='Description' rows='5' id="description" required/>
-                <input className='w-100 rounded' type="text" placeholder='Endereço' id='address' required/>
+                <input 
+                  placeholder='Nome' 
+                  id='name'
+                  onChange={handleChange}
+                  value={formData.name} 
+                  className='w-100 mb-2 rounded' 
+                  type="text"  maxLength='10' 
+                  minLength='50' required
+                />
+                <textarea 
+                  placeholder='Description'
+                  id="description"
+                  onChange={handleChange}
+                  value={formData.description}
+                  className='w-100 mb-2 rounded' 
+                  type="text"  
+                  rows='5'  required
+                />
+                <input 
+                  placeholder='Endereço' 
+                  id='address' 
+                  onChange={handleChange}
+                  value={formData.address}
+                  className='w-100 rounded' 
+                  type="text"  required
+                />
 
                 {/* Dados do Imóvel */}
                 <div className='d-flex'>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-check-input" type="checkbox" role="switch" id="rent"/>
+                        <input onChange={handleChange} checked={formData.type === 'rent'} className="form-check-input" type="checkbox" role="switch" id="rent"/>
                         <label className="form-check-label" htmlFor="rent">Alugar</label>
                     </div>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-check-input" type="checkbox" role="switch" id="sell"/>
+                        <input onChange={handleChange} checked={formData.type === 'sell'} className="form-check-input" type="checkbox" role="switch" id="sell"/>
                         <label className="form-check-label" htmlFor="sell">Vender</label>
                     </div>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-check-input" type="checkbox" role="switch" id="parkingSpots"/>
+                        <input onChange={handleChange} checked={formData.parking} className="form-check-input" type="checkbox" role="switch" id="parkingSpots"/>
                         <label className="form-check-label" htmlFor="parkingSpots">Vaga de garagem</label>
                     </div>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-check-input" type="checkbox" role="switch" id="furnished"/>
+                        <input onChange={handleChange} checked={formData.furnished} className="form-check-input" type="checkbox" role="switch" id="furnished"/>
                         <label className="form-check-label" htmlFor="furnished">Mobiliado</label>
                     </div>
                 </div>
                 <div class="form-check form-switch text-start m-4">
-                    <input className="form-check-input" type="checkbox" role="switch" id="Offer"/>
+                    <input onChange={handleChange} checked={formData.offer} className="form-check-input" type="checkbox" role="switch" id="Offer"/>
                     <label className="form-check-label" htmlFor="Offer">Oferta</label>
                 </div>
 
                 {/* Dados a serem preenchidos */}
                 <div className='d-flex '>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-input mx-1" type="number" id="Bedrooms" style={{ width: "50px" }}/>
+                        <input onChange={handleChange} value={formData.bedrooms} className="form-input mx-1" type="number" id="Bedrooms" style={{ width: "50px" }}/>
                         <label className="form-label" htmlFor="CheckDefault">Quartos</label>
                     </div>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-input mx-1" type="number" id="bathrooms" style={{ width: "50px" }}/>
+                        <input onChange={handleChange} value={formData.bathrooms} className="form-input mx-1" type="number" id="bathrooms" style={{ width: "50px" }}/>
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Banheiros</label>
                     </div>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-input mx-1" type="number" id="rentValue" style={{ width: "50px" }}/>
+                        <input onChange={handleChange}
+                          value={formData.regularPrice}
+                          min='100' 
+                          max='1000000' 
+                          className="form-input mx-1" 
+                          type="number" 
+                          id="rentValue" 
+                          style={{ width: "50px" }}
+                        />
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Preço Padrão (R$/mês)</label>
                     </div>
                     <div class="form-check form-switch text-start m-4">
-                        <input className="form-input mx-1" type="number" id="rentDiscounted" style={{ width: "50px" }}/>
+                        <input onChange={handleChange}
+                          value={formData.discountPrice}
+                          className="form-input mx-1" 
+                          type="number" 
+                          id="rentDiscounted" 
+                          style={{ width: "50px" }}
+                        />
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Desconto (R$/mês)</label>
                     </div>
                 </div>
@@ -167,11 +283,15 @@ export default function CreateList() {
                 </button>
               </div>
             ))}
-          <button className=''>
-            Criar listagem
-          </button>
-            </div>
+            <button
+              disabled={loading || uploading}
+              className=''
+            >
+              {loading ? 'Creating...' : 'Create listing'}
+            </button>
+            {error && <p className=''>{error}</p>}
+          </div>
         </div>
-    </section>
+    </form>
   )
 }
