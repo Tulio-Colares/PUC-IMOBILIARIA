b0d506c2195feb986deb8b3ed3407defafdaec54
Tulio <tuliocsaraiva@gmail.com>
2024-05-09 15:02:12 -0300

    criando rota de atualização de listagem

--- server/controllers/list.js
+++ server/controllers/list.js
@@ -1,4 +1,5 @@
 import List from '../models/list.js';
+import { errorHandler } from '../utils/error.js';
 
 export const createList = async (req, res, next) => {
   try {
@@ -23,6 +24,27 @@ export const deleteList = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listagem deletada!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateList = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listagem não encontrada'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'Você só pode deletar suas próprias listagens'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
--- server/routes/list.js
+++ server/routes/list.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createList,  deleteList } from '../controllers/list.js';
+import { createList,  deleteList, updateList } from '../controllers/list.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/createToken', verifyToken, createList);
 router.delete('/deleteToken/:id', verifyToken, deleteList);
+router.post('/update/:id', verifyToken, updateList);
 
 export default router;
