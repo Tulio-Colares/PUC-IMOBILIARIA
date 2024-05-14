7706b89cf6a378635d622187655665bc653a37c3
Tulio <tuliocsaraiva@gmail.com>
2024-05-14 12:01:34 -0300

    criando rota de busca no backend

--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -62,9 +62,9 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+          <div className=''>
             <FaShare
-              className='text-slate-500'
+              className=''
               onClick={() => {
                 navigator.clipboard.writeText(window.location.href);
                 setCopied(true);
@@ -89,7 +89,7 @@ export default function Listing() {
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className=''>
-              <FaMapMarkerAlt className='text-green-700' />
+              <FaMapMarkerAlt className='' />
               {listing.address}
             </p>
             <div className=''>
--- server/routes/list.js
+++ server/routes/list.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createList, getList, updateList, deleteList } from '../controllers/list.js';
+import { createList, getList, updateList, deleteList, getLists } from '../controllers/list.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/createToken', verifyToken, createList);
 router.get('/getToken/:id', getList);
 router.post('/updateToken/:id', verifyToken, updateList);
 router.delete('/deleteToken/:id', verifyToken, deleteList);
+router.get('/get', getLists);
 
 
 export default router;
